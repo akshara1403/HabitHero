8f8477a73e84d3663eb61d89f574dd6531b149f4
akshara1403 <160697682+akshara1403@users.noreply.github.com>
2025-08-07 05:57:48 +0530

    Add files via upload

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HabitList from "../components/HabitList";
+
+function Home() {
+  return (
+    <div>
+      
+      <HabitList />
+    </div>
+  );
+}
+
+export default Home;
